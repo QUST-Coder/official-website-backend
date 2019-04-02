49b393913369243231f6eb8b24684b16de04eb72
RBWang <x7536110@outlook.com>
2019-04-03 00:45:03 +0800

    add handler loader

---
+++ handler/index.js
@@ -0,0 +1,38 @@
+const BaseHandler = require("../base/base_handler");
+const path = require("path");
+const fs = require("fs");
+class HandlerLoader extends BaseHandler {
+    constructor() {
+        super(...arguments);
+        this.__registHandler();
+    }
+    __registHandler() {
+        this.handlerMap = {};
+        let files = fs.readdirSync(__dirname);
+        files.forEach(file => {
+            if (file === "index.js" || path.extname(file) !== "js") return;
+            let handler = new require(path.join(__dirname, file))();
+            let funcNames = Object.keys(handler.__proto__).filter(name => {
+                return typeof this.__proto__[name] === "function";
+            });
+            funcNames.forEach(func => {
+                if (!this.handlerMap[func]) {
+                    this.handlerMap[func] = handler;
+                } else {
+                    throw new Error("depulicate funcName, funcName:" + func);
+                }
+            });
+            this.logger.info(`handler ${file}\tload success.`);
+        });
+    }
+    getHandler(funcName) {
+        if (this.handlerMap[funcName]) {
+            return this.handlerMap[funcName];
+        } else {
+            let error = new this.Error(1000, "Invalid function name, name:" + funcName);
+            throw error;
+        }
+    }
+}
+
+module.exports = new HandlerLoader();
---
+++ handler/loginHandler.js
@@ -0,0 +1,21 @@
+const BaseHandler = require("../base/base_handler");
+
+class LoginHandler extends BaseHandler {
+    constructor() {
+        super(...arguments);
+    }
+    //TODO
+    // register(username, nickname, email, password, token) {
+
+    // }
+
+    login() {
+
+    }
+
+    logout() {
+
+    }
+
+}
+module.exports = LoginHandler;
