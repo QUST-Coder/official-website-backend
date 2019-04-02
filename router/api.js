e9ccfcd86c1c806daf980cabdff7a56b5b9b626a
RBWang <x7536110@outlook.com>
2019-04-03 00:44:43 +0800

    add api loader

---
+++ router/api.js
@@ -0,0 +1,39 @@
+const BaseRouter = require("../base/base_router");
+const Handler = require("../handler");
+const validate = require("../schema");
+class ApiRouter extends BaseRouter {
+    constructor() {
+        super(...arguments);
+    }
+    async postApi(ctx) {
+        try {
+            let { body: req } = ctx.req;
+            let { func, args } = req;
+            this.logger.info(`req|requestId=${ctx.requestId}|body=${JSON.stringify(req)}`);
+            let handler = Handler.getHandler(func);
+            args = validate(func, args);
+            let rsp = await handler[func](...args);
+            this.logger.info(`rsp|requestId=${ctx.requestId}|body=${JSON.stringify(rsp)}`);
+            ctx.body = rsp;
+        } catch (err) {
+            if (err.code) {
+                ctx.body = {
+                    error: {
+                        code: err.code,
+                        msg: err.msg
+                    }
+                };
+            } else {
+                ctx.body = {
+                    error: {
+                        code: -1,
+                        msg: "system error"
+                    }
+                };
+            }
+            this.logger.error(`err|requestId=${ctx.requestId}|code=${err.code || -1}|message=${err.message}`);
+        }
+    }
+}
+
+module.exports = ApiRouter;
