bae0bc43424b0b7b24301c27e8fa347e613e3704
richbxwang <richbxwang@tencent.com>
2019-04-03 23:45:55 +0800

    add schema

--- schema/index.js
+++ schema/index.js
@@ -1,3 +1,17 @@
-module.exports = (func,args)=>{
-    return args;
+const Joi = require("joi");
+const fs = require("fs");
+const path = require("path");
+const validates = {};
+fs.readdirSync(__dirname).forEach(file => {
+    if (file == "index.js" || path.extname(file) !== ".js") return;
+    validates[file.split("_")[0]] = require(path.join(__dirname, file));
+});
+module.exports = (func, args) => {
+    let validate = validates[func];
+    let { error, value } = Joi.validate(args, validate);
+    if (error !== null) {
+        //console.log(err);
+        throw error;
+    }
+    return value;
 };
