38aa136f9276467a41ea0ce860505bc74ba028d4
richbxwang <richbxwang@tencent.com>
2019-04-03 18:04:02 +0800

    remove 996.icu license

---
+++ handler/captchaHandler.js
@@ -0,0 +1,16 @@
+const BaseHandler = require("../base/base_handler");
+
+class capthaHandler extends BaseHandler {
+    constructor() {
+        super(...arguments);
+    }
+    async genNewCode(pin) {
+
+    }
+
+    async verifyCode() {
+
+    }
+
+}
+module.exports = capthaHandler;
