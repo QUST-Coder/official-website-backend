b54404f5977d3bc8340e2786a89ccb5c9fd23665
RBWang <x7536110@outlook.com>
2019-04-05 16:32:45 +0800

    init captchaHandler

---
+++ handler/captcha_handler.js
@@ -0,0 +1,47 @@
+"use strict";
+const BaseHandler = require("../base/base_handler");
+class captchaHandler extends BaseHandler {
+    constructor() {
+        super(...arguments);
+    }
+
+    async getCaptcha(pin) {
+        try {
+            this.logger.info(pin);
+            return {
+                error: {
+                    code: 0,
+                    msg: "success"
+                },
+                error: {
+                    code: 0,
+                    msg: ""
+                },
+                img: ""
+            };
+        } catch (err) {
+            this.logger.error(err);
+        }
+    }
+
+
+    async checkCaptcha(pin, token) {
+        try {
+            this.logger.info(pin, token);
+            return {
+                error: {
+                    code: 0,
+                    msg: "success"
+                },
+                error: {
+                    code: 0,
+                    msg: ""
+                }
+            };
+        } catch (err) {
+            this.logger.error(err);
+        }
+    }
+
+}
+module.exports = captchaHandler;
